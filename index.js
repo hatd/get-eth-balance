84365dfcf883a38d3b3b5bc7b807bb9c72c5b8fe
hatd <ennino1234@gmail.com>
2024-03-08 11:13:01 +0700

    update rpc

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import Web3HttpProvider from 'web3-providers-http';
 const DEFAULT_RPC = 'https://rpc.ankr.com/eth'
 
 class GetEthBalance {
-    async getBalance(address, rpc = DEFAULT_RPC, libName = undefined) {
-        const web3 = new Web3(new Web3HttpProvider(rpc));
+    async getBalance(address, libName = undefined) {
+        const web3 = new Web3(new Web3HttpProvider(DEFAULT_RPC));
         const balance = await web3.eth.getBalance(address)
         return web3.utils.fromWei(balance, 'ether')
     }
